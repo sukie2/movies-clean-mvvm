576c5501aaf9962cebed4c6d488f2bce35e901a3
Suki <tmpuw3@sg.uob>
2020-05-22 21:21:43 +0800

    Minor formatting

--- app/src/main/java/com/sukie2/android/movies/framework/networking/RequestInterceptor.kt
+++ app/src/main/java/com/sukie2/android/movies/framework/networking/RequestInterceptor.kt
@@ -9,7 +9,7 @@ class RequestInterceptor : Interceptor {
         val originalRequest = chain.request()
         val originalUrl = originalRequest.url
 
-        // add TheMovieDB api automatically every requests.
+        //add TheMovieDB api automatically every requests.
         val url = originalUrl.newBuilder()
             .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
             .build()
